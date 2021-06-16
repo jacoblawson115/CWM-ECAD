ffd07f3757ad7a9cd93c806b6c7a21933d189343
Cloud User <centos@ip-172-31-4-251.eu-west-2.compute.internal>
2021-06-16 13:16:45 +0000

    Exercise 5 solution

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Jake Lawson
+// Date: 16/06/2021
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -13,3 +13,46 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+module ac (
+// declare inputs
+    input clk, [4:0]temperature, initState
+    output reg heating,
+    output reg cooling,
+    );
+
+// define an internal state variable and keep it in a register
+    wire [1:0]st;
+    reg [1:0]state;
+    assign st = initState; //define initial state
+    state = st;
+
+// logic to determine on clk edge the next state from temperature input and current state
+    always @ (posedge clk)
+       begin
+          if(state==2'b11 || state==2'bx) //illegal states - reject, go to idle
+             assign st = 2'b00;
+          end
+
+          if(state==2'b00) //idle state
+             assign st = (temperature >= 5'b10110) ? 2'b01: 
+                    (temperature <= 5'10010) ? 2'b10:
+                    2'b00;
+          end
+
+          if(state==2'b01) //cooling state
+             assign st = (temperature <= 5'10100) ? 2'b00: 
+                    2'b01;
+          end
+ 
+          if(state==2'b10) //heating state
+             assign st = (temperature >= 5'10100) ? 2'b00: 
+                    2'b10;
+          end
+
+          state = st; //update state to new state
+          heating = state[1];
+          cooling = state[0];
+       end
+
+
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -7,3 +7,56 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+module top_tb(
+   //No ports are defined! 
+   );
+
+    //Parameters    
+    parameter CLK_PERIOD = 10;
+
+    //Registers
+    reg clk;
+    reg [4:0]temperature;
+    reg initState; //define an initial state so we can make sure our implementation works with all initial states and rejects illegal states
+
+    //Wires
+    wire heating;
+    wire cooling;
+
+    //Create clock signal
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+    end
+
+    //Create temperature signal (use different signals depending on initial state so we can cycle through states in order)
+    initial
+    begin
+       if(initState == 2'b00 || initState == 2'b11):
+       end
+
+       if(initState == 2'b01):
+       end
+
+       if(initState == 2'b10):
+       end
+
+
+       temperature = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+    end
+    
+
+//The instantiation of the user's module
+     ac mymodule (
+     .clk (clk),
+     .initState (initState),
+     .temperature (temp),
+     .heating (heating),
+     .cooling (cooling),
+     );
+
+endmodule
