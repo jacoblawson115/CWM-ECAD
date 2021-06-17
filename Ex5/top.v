829bbda1674e38c8d4ae142e364e8bb40332f7db
Cloud User <centos@ip-172-31-4-251.eu-west-2.compute.internal>
2021-06-17 09:07:15 +0000

    Exercise 5 solution

--- Ex5/top.v
+++ Ex5/top.v
@@ -16,40 +16,39 @@
 module ac (
 // declare inputs
     input clk, [4:0] temperature, [1:0] initState,
-    output reg heating, 
-    output reg cooling
+    output heating, cooling
     );
 
 // define an internal state variable and keep it in a register
-    wire [1:0] initState;
     reg [1:0] state; 
-    state = initState; //define initial state
+    state <= initState; //define initial state
+
+    assign heating = state[0];
+    assign cooling = state[1];
 
 // logic to determine on clk edge the next state from temperature input and current state
     always @ (posedge clk) begin
 
           if(state==2'b11 || state==2'bx) begin //illegal states - reject, go to idle
-             state = 2'b00;
+             state <= 2'b00;
           end
 
           if(state==2'b00) begin //at idle state
-             state = (temperature >= 5'd22) ? 2'b01 : 
+             state <= (temperature >= 5'd22) ? 2'b01 : 
                      (temperature <= 5'd18) ? 2'b10 :
                      2'b00;
           end
 
           if(state==2'b01) begin //at cooling state
-             state = (temperature <= 5'd20) ? 2'b00 : 
+             state <= (temperature <= 5'd20) ? 2'b00 : 
                      2'b01;
           end
  
           if(state==2'b10) begin //at heating state
-             state = (temperature >= 5'd20) ? 2'b00 : 
+             state <= (temperature >= 5'd20) ? 2'b00 : 
                      2'b10;
           end
 
-          heating = state[1];
-          cooling = state[0];
        end
 
 
