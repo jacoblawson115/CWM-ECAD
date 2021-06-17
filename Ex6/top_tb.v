fe0d0c96eb008d08cb23c4f3c5c74f7037080b68
Cloud User <centos@ip-172-31-4-251.eu-west-2.compute.internal>
2021-06-17 16:01:01 +0000

    Exercise 6 Solution

--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -7,3 +7,35 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+   //No ports are defined! 
+   );
+
+    //Parameters    
+    parameter CLK_PERIOD = 10;
+
+    //Registers
+    reg rst;
+    reg clk;
+    reg button;
+    reg err;
+    reg [2:0] prv_val; //register to save the previous value
+
+    //Wires
+    wire [2:0] colour;
+
+
+
+
+	//The instantiation of the user's module
+     rgb_converter mymodule (
+     .clk (clk),
+     .colour (colour),
+     .enable (enable),
+     .rgb (rgb)
+     );
+
+endmodule
+	
